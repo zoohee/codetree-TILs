989d7a5fd6b9be816baf910f291ef419768046a8
JUHEE LEE <48899055+zoohee@users.noreply.github.com>
2024-07-19 14:36:49 +0900

    Title: 삼각형 컨베이어 벨트, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240719/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+
+graph = []
+for _ in range(n):
+    graph += list(map(int, input().split()))
+
+def rotate():
+    temp = []
+    for i in range(1, m+1):
+        temp.append(graph[-i])
+
+    for i in range(len(graph)-1, -1, -1):
+        graph[i] = graph[i-m]
+    
+    for i in range(m-1, -1, -1):
+        graph[i] = temp[i-m+1]
+
+rotate()
+for i in range(len(graph)):
+    if i!= 0 and i % n == 0:
+        print()
+    print(graph[i], end = " ")
