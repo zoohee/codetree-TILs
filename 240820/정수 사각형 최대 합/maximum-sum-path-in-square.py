68217f7e2e74c65bc1552ae620d1e62f6304bf0f
JUHEE LEE <48899055+zoohee@users.noreply.github.com>
2024-08-20 13:44:02 +0900

    Title: 정수 사각형 최대 합, Time: 196ms, Memory: 29MB, Status: Passed - CodeTree

--- 240820/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240820/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -1,11 +1,10 @@
 import copy
 n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
+graph = [[0] * (n+1)] + [[0]+list(map(int, input().split())) for _ in range(n)]
 dp = copy.deepcopy(graph)
 
-for i in range(1, n):
-    for j in range(n-1):
-        for k in range(i):
-            dp[i][j] = max(dp[i][j], dp[i][j]+dp[i-1][k])
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp[i][j] = graph[i][j] + max(dp[i-1][j], dp[i][j-1])
 
-print(max(dp[n-1]))
+print(dp[n][n])
